ccd6d3a151a8bd384f761d9c3f4804bad19ca3fa
dmswl0311 <eunji.cho2@cj.net>
2023-09-12 20:52:46 +0900

    [develop-openFeign] feat: openFeign 공공 api 호출 테스트

--- backend/toy/src/main/java/com/cej/toy/openApi/service/OpenApiService.java
+++ backend/toy/src/main/java/com/cej/toy/openApi/service/OpenApiService.java
@@ -1,15 +1,49 @@
 package com.cej.toy.openApi.service;
 
+import com.cej.toy.openApi.api.OpenApiFeignClient;
+import com.cej.toy.openApi.domain.InquiryInformationResponse;
+import com.cej.toy.openApi.domain.InquiryListNationalQualifcationResponse;
+import com.cej.toy.openApi.domain.ItemDto;
+import feign.codec.DecodeException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class OpenApiService {
+    private final OpenApiFeignClient openApiFeignClient;
 
-    public Object getList(){
-        return null;
+    @Value("${open-api-key}") String ServiceKey;
+
+    public Object getCertInfo(String jmcd){
+        Object result = null;
+        try {
+            ResponseEntity<InquiryInformationResponse> response = openApiFeignClient.getCertInfo(ServiceKey, jmcd);
+            result = Objects.requireNonNull(response.getBody()).getResponse().getBody().getItems().getItem();
+        } catch (DecodeException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public Object getCertMetaInfo(String jmcd){
+        Object result = null;
+        try {
+            ResponseEntity<InquiryListNationalQualifcationResponse> response = openApiFeignClient.getCertMetaInfo(ServiceKey);
+            List<ItemDto> items = Objects.requireNonNull(response.getBody()).getResponse().getBody().getItems().getItem();
+            result = items.stream().filter(item -> item.getJmcd().equals(jmcd)).toList();
+        } catch (DecodeException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
